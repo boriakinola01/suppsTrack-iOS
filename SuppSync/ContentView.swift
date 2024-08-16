5169ec3776b1a7c0c40699485bb7724c220331ad
Bori Akinola <boriakinola01@gmail.com>
2024-08-16 09:50:26 +0100

    Initial Commit

---
+++ SuppSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuppSync
+//
+//  Created by Bori Akinola on 16/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuppSync/SuppSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuppSyncApp.swift
+//  SuppSync
+//
+//  Created by Bori Akinola on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuppSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
